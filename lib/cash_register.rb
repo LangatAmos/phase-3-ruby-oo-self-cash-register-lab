7c06c0a2a0ed96965a5d030e4adb68fbb3c28689
Amos Langat <amos.langat@student.moringaschool.com>
2022-10-21 12:44:57 +0300

    complete assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
+require 'pry'
+class CashRegister
+    attr_accessor :total, :discount, :last_transaction
 
+    def initialize(discount=0, total=0 , items=[])
+        @total = total
+        @discount = discount 
+        @items = items
+    end
+
+    def total
+        @total
+    end
+
+    def add_item(title, price, qty=1)
+        @total += price * qty
+        qty.times do 
+            @items.push title
+        end
+        @last_transaction = qty * price
+    end
+
+    def apply_discount
+        @total -= @total * self.discount/100
+        if self.discount > 0 
+            "After the discount, the total comes to $#{@total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+    def items
+        @items
+    end
+
+    def void_last_transaction
+        @total -= self.last_transaction
+    end
+end
